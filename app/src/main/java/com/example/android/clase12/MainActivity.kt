c640f49f4baa55ab6c17ddd7ee461e8aa464a5e8
toxocarrier <catalinapimentel@gmail.com>
2018-12-13 19:08:03 -0300

    creando custom adapter

--- app/src/main/java/com/example/android/clase12/MainActivity.kt
+++ app/src/main/java/com/example/android/clase12/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.android.clase12
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +16,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+    }
+
+    //como el customadapter se usara solo una vez, lo creamos en el main activity
+    class CustomAdapter(var miContexto:Context,
+                        var miRecurso:Int,
+                        var miLista:ArrayList<Serie>):
+        ArrayAdapter<Serie>(miContexto,miRecurso,miLista){
+        override fun getView(position:Int,convertView: View?, parent: ViewGroup): View {
+            var v = LayoutInflater.from(miContexto).inflate(miRecurso, null)
+            var valor = v.findViewById<TextView>(R.id.lblValor)
+            var fecha = v.findViewById<TextView>(R.id.lblFecha)
+            valor.text = miLista[position].valor.toString()
+            fecha.text=miLista[position].fecha
+            return v
+        }
     }
 }
